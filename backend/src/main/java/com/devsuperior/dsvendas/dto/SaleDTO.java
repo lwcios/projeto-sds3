8f2543e53e6a87c20d7cba9ec5fa955a9b127c81
lwcios <lwciomawro@hotmail.com>
2021-05-06 14:48:55 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dsvendas/dto/SaleDTO.java
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SaleDTO.java
@@ -30,12 +30,12 @@ public class SaleDTO {
 	
 public SaleDTO(Sale entity ) {
 		
-		id = entity.getId();
-		visited = entity.getVisited();
-		deals = entity.getDeals();
-		amount = entity.getAmount();
-		date = entity.getDate();
-		seller = new SellerDTO( entity.getSeller());
+	this.id = entity.getId();
+	this.visited = entity.getVisited();
+	this.deals = entity.getDeals();
+	this.amount = entity.getAmount();
+	this.date = entity.getDate();
+	this.seller = new SellerDTO( entity.getSeller());
 	}
 
 public Long getId() {
